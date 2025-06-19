b86749332575a705d4e8e488fd3f066ddb2a3f6e
chaeb03 <20231882@daejin.ac.kr>
2025-06-19 17:18:57 +0900

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,7 +1,8 @@
-window.onload = function() {
+window.onload = () => {
   window.ui = SwaggerUIBundle({
-    url: "swagger.yaml", 
+    url: "./swagger.yaml",  // API 스펙이 정의된 YAML 파일 경로
     dom_id: '#swagger-ui',
+    deepLinking: true,
     presets: [
       SwaggerUIBundle.presets.apis,
       SwaggerUIStandalonePreset
